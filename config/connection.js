d9083d24ea8ba2f4d27ee4fb1bf611607d470b9a
andrewburd <andrewburd2@gmail.com>
2018-03-19 16:17:48 -0400

    first timer

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	port: 8889,
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'burgers_db'
+})
+
+connection.connect(function(err){
+	if(err)throw err;
+	console.log("connected as id: " +connection.threadid);
+});
+
+module.exports = connection;

49244f6cfe1d321248c45e24495bf6e50b59bd39
andrewburd <andrewburd2@gmail.com>
2018-03-21 10:51:29 -0400

    working for now

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 8889,
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'burgers_db'
-})
+// Set up MySQL connection.
+var mysql = require("mysql");
 
-connection.connect(function(err){
-	if(err)throw err;
-	console.log("connected as id: " +connection.threadid);
+var connection = mysql.createConnection({
+  port: 8889,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db"
 });
 
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id: " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,74 @@
-var connection = require('./connection.js');
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
 
 var orm = {
-	all: function(tableInput, cb){
-		connection.query('SELECT * FROM ' + tableInput + ';', function(err,result){
-			if(err) throw err;
-			cb(result)
-		})
-	},
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
-	update: function(tableInput, condition, cb){
-		connection.query('UPDATE '+tableInput+ 'SET devoured=true WHERE id='+condition+';', function(err,result){
-				if(err) throw err;
-				cb(result);
-			})
-	},
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
-	create: function(tableInput,val,cb){
-		connection.query('INSERT INTO '+tableInput+" (burger_name) VALUES ('"+val+"'):", function(err,result){
-			if(err)throw err;
-			cb(result);
-		})
-	}
-}
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
 
 module.exports = orm;
